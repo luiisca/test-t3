db081d2f99c081bf36452fac6b6e444243f4486c
Luis Cadillo <luiscaf3r@gmail.com>
2024-02-07 12:07:50 -0500

    fix comment client comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default async function Home() {
                     </div>
                 </div>
 
-                // <CrudShowcase />
+                {/* <CrudShowcase /> */}
             </div>
         </main>
     );
