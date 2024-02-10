babbff849c20195be8665819b690f965264189fe
Luis Cadillo <luiscaf3r@gmail.com>
2024-02-09 22:03:20 -0500

    test auth

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,59 +1,59 @@
-// import { headers } from "next/headers";
+import { headers } from "next/headers";
 
-// import { env } from "~/env";
+import { env } from "~/env";
 // import { redirect } from "next/navigation";
 import { getServerAuthSession } from "~/server/auth";
 
 export default async function Login() {
     const session = await getServerAuthSession();
 
+    // if (session?.user) {
+    //     return redirect('/simulation')
+    //     // return (
+    //     //     <p>
+    //     //         Authenticated!
+    //     //     </p>
+    //     // )
+    // } else {
+    //     return <p>Not Authenticated</p>
+    // }
     if (session?.user) {
         // return redirect('/simulation')
         return (
             <p>
-                Authenticated!
+                Not Authenticated!
             </p>
         )
     } else {
-        return <p>Not Authenticated</p>
+        const csrfToken = (await fetch(`${env.NEXTAUTH_URL}/api/auth/csrf`, {
+            headers: headers()
+        }).then(res => res.json()) as {
+            csrfToken: string
+        }).csrfToken
+
+        return (
+            <div>
+                <p>Authenticated!</p>
+                <form
+                    method="POST"
+                    action={`${env.NEXTAUTH_URL}/api/auth/signin/github`}
+                    className="flex flex-col group gap-2">
+
+                    <input
+                        hidden
+                        value={csrfToken}
+                        name="csrfToken"
+                        readOnly />
+
+                    <button
+                        className="outline-none 
+            focus:underline focus:decoration-red-600 
+            focus:group-valid:decoration-green-600">
+                        Log in
+                    </button>
+                </form>
+            </div>
+        )
     }
-    // if (session?.user) {
-    //     // return redirect('/simulation')
-    //     return (
-    //         <p>
-    //             Not Authenticated!
-    //         </p>
-    //     )
-    // } else {
-    //     const csrfToken = (await fetch(`${env.NEXTAUTH_URL}/api/auth/csrf`, {
-    //         headers: headers()
-    //     }).then(res => res.json()) as {
-    //         csrfToken: string
-    //     }).csrfToken
-    //
-    //     return (
-    //         <div>
-    //             <p>Authenticated!</p>
-    //             <form
-    //                 method="POST"
-    //                 action={`${env.NEXTAUTH_URL}/api/auth/signin/github`}
-    //                 className="flex flex-col group gap-2">
-    //
-    //                 <input
-    //                     hidden
-    //                     value={csrfToken}
-    //                     name="csrfToken"
-    //                     readOnly />
-    //
-    //                 <button
-    //                     className="outline-none 
-    //         focus:underline focus:decoration-red-600 
-    //         focus:group-valid:decoration-green-600">
-    //                     Log in
-    //                 </button>
-    //             </form>
-    //         </div>
-    //     )
-    // }
 }
 
