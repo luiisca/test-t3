e7b20f95d9b34d8cd14ddbeeb147132e013478f5
Luis Cadillo <luiscaf3r@gmail.com>
2024-02-09 22:08:58 -0500

    test auth

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -25,33 +25,34 @@ export default async function Login() {
             </p>
         )
     } else {
-        const csrfToken = (await fetch(`${env.NEXTAUTH_URL}/api/auth/csrf`, {
-            headers: headers()
-        }).then(res => res.json()) as {
-            csrfToken: string
-        }).csrfToken
+        // const csrfToken = (await fetch(`${env.NEXTAUTH_URL}/api/auth/csrf`, {
+        //     headers: headers()
+        // }).then(res => res.json()) as {
+        //     csrfToken: string
+        // }).csrfToken
 
         return (
             <div>
                 <p>Authenticated!</p>
-                {/*     <form */}
-                {/*         method="POST" */}
-                {/*         action={`${env.NEXTAUTH_URL}/api/auth/signin/github`} */}
-                {/*         className="flex flex-col group gap-2"> */}
-                {/**/}
-                {/*         <input */}
-                {/*             hidden */}
-                {/*             value={csrfToken} */}
-                {/*             name="csrfToken" */}
-                {/*             readOnly /> */}
-                {/**/}
-                {/*         <button */}
-                {/*             className="outline-none  */}
-                {/* focus:underline focus:decoration-red-600  */}
-                {/* focus:group-valid:decoration-green-600"> */}
-                {/*             Log in */}
-                {/*         </button> */}
-                {/*     </form> */}
+                <form
+                    method="POST"
+                    action={`${env.NEXTAUTH_URL}/api/auth/signin/github`}
+                    className="flex flex-col group gap-2">
+
+                    <input
+                        hidden
+                        // value={csrfToken}
+                        value='hey'
+                        name="csrfToken"
+                        readOnly />
+
+                    <button
+                        className="outline-none 
+                focus:underline focus:decoration-red-600 
+                focus:group-valid:decoration-green-600">
+                        Log in
+                    </button>
+                </form>
             </div>
         )
     }
