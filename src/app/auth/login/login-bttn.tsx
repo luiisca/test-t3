1bfe4ded192807b8315e334e9a20d2271951e4a5
Luis Cadillo <luiscaf3r@gmail.com>
2024-02-10 22:49:04 -0500

    test auth

--- src/app/auth/login/login-bttn.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { signIn } from "next-auth/react"
-
-export default function() {
-    return (
-        <button onClick={() => signIn('github')}>Login with Github</button>
-    )
-}
