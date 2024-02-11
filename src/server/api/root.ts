bc241a895d4eb4b9f5ad1e97cf77c69f1673ea25
Luis Cadillo <luiscaf3r@gmail.com>
2024-02-10 21:28:59 -0500

    test auth

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+// import { postRouter } from "~/server/api/routers/post";
 // import { externalRouter } from "~/server/api/routers/externalRouter";
 import { createTRPCRouter } from "~/server/api/trpc";
 
@@ -8,7 +8,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-    post: postRouter,
+    // post: postRouter,
     // external: externalRouter,
 });
 
